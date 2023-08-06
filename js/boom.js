1b80b77e41e8020f11760f7c491d62412a8e1ed2
YubaC <2383931696@qq.com>
2023-08-06 18:24:40 +0800

    Feat: 借助BUD实现对无限地图的适配

--- js/boom.js
+++ js/boom.js
@@ -1,9 +1,160 @@
-class Boom {
+// class Boom {
+//     constructor(config) {
+//         this.range = config.range || 2; // Needs to config
+//         this.x = config.x; // Needs to config
+//         this.y = config.y; // Needs to config
+//         this.map = config.map; // Needs to config
+
+//         this.name = "💣";
+//         this.type = "boom";
+//         this.damage = config.damage || 10; // 爆炸伤害
+//         this.damageReduction = config.damageReduction || 0.6; // 爆炸伤害衰减
+//         this.damagePriority = config.damagePriority || 1000; // 攻击优先级，参见docs/attack.md
+//         this.damageList = config.damageList || ["all"]; // 可以伤害的实体类型
+//         this.taskPriority = 1000; // 决定在每tick中的更新顺序，数字越大越先更新
+//         this.boomed = false;
+//         this.boomedInThisTick = false; // 在本tick中是否已经爆炸
+//         this.boomedByOther = false; // 在被别的炸弹引爆后用于统一清除时间
+//         this.finished = false;
+//         this.ticksBrforeBoom = config.ticksBrforeBoom || 15;
+//         this.ticksBrforeClear = config.ticksBrforeClear || 5;
+//         this.skipUpdate = 0; // 用于跳过更新的tick数
+//     }
+
+//     /**
+//      * 初始化的函数
+//      */
+//     setup() {
+//         // 向entities中添加自身
+//         this.map.entities.push(this);
+//     }
+
+//     /**
+//      * 负责在新的tick中更新状态的函数
+//      * @memberof Boom
+//      * @returns {boolean} 是否需要更新地图
+//      */
+//     update() {
+//         // 跳过更新
+//         if (this.skipUpdate > 0) {
+//             this.skipUpdate--;
+//             return false;
+//         }
+
+//         if (this.boomedInThisTick) {
+//             this.boomedInThisTick = false;
+//         }
+
+//         if (this.boomed) {
+//             if (this.ticksBrforeClear <= 0) {
+//                 this.clearBoom();
+//                 return true;
+//             }
+//             this.ticksBrforeClear--;
+//         } else {
+//             if (this.ticksBrforeBoom <= 0) {
+//                 this.boom();
+//                 return true;
+//             }
+//             this.ticksBrforeBoom--;
+//         }
+//     }
+
+//     /**
+//      * 炸弹爆炸
+//      * @memberof Boom
+//      * @returns {void}
+//      * @description 炸弹爆炸后，会在爆炸范围内的所有地块上生成“炸”，然后变成“土”
+//      */
+//     boom() {
+//         this.boomed = true;
+//         this.boomedInThisTick = true;
+//         this.name = "";
+//         this.ticksBrforeBoom = 0;
+//         const { blocks, entities } = this.map;
+//         const { range, x, y } = this;
+
+//         for (let i = x - range; i <= x + range; i++) {
+//             for (let j = y - range; j <= y + range; j++) {
+//                 if (this.map.isInMap(i, j)) {
+//                     blocks[i][j].name = "炸";
+//                     entities.forEach((entity) => {
+//                         // 引爆别的炸弹
+//                         if (entity.x === i && entity.y === j) {
+//                             if (entity.type === "boom" && !entity.boomed) {
+//                                 entity.boom();
+//                                 entity.skipUpdate += 1;
+//                             }
+//                         }
+//                     });
+//                 }
+//             }
+//         }
+//     }
+
+//     canHurt(entity) {
+//         // 检查实体是否能被伤害
+//         if (!this.boomedInThisTick) {
+//             return false;
+//         }
+//         if (
+//             !this.damageList.includes(entity.type) &&
+//             !this.damageList.includes("all")
+//         ) {
+//             return false;
+//         }
+//         // 计算实体和炸弹是否行和列之间的距离小于等于炸弹的爆炸范围
+//         const distanceX = Math.abs(entity.x - this.x);
+//         const distanceY = Math.abs(entity.y - this.y);
+//         const distance = Math.max(distanceX, distanceY);
+//         return distance <= this.range;
+//     }
+
+//     hurt(entity) {
+//         if (!this.canHurt(entity)) {
+//             return false;
+//         }
+
+//         const distance =
+//             Math.abs(this.x - entity.x) + Math.abs(this.y - entity.y);
+//         const damage = Math.floor(
+//             this.damage * Math.pow(this.damageReduction, distance)
+//         );
+//         return damage;
+//     }
+
+//     clearBoom() {
+//         // console.log(`clear x:${this.x}, y:${this.y} boom`);
+//         // 遍历爆炸范围内的所有地块
+//         for (let x = this.x - this.range; x <= this.x + this.range; x++) {
+//             for (let y = this.y - this.range; y <= this.y + this.range; y++) {
+//                 // 如果地块在地图范围内，则在地块上生成“土”
+//                 if (
+//                     this.map.isInMap(x, y) &&
+//                     this.map.blocks[x][y].name === "炸"
+//                 ) {
+//                     this.map.blocks[x][y].name = "土";
+//                 }
+//             }
+//         }
+
+//         this.finished = true;
+//         // 从entities中删除自身
+//         for (let i = 0; i < this.map.entities.length; i++) {
+//             const entity = this.map.entities[i];
+//             if (entity === this) {
+//                 this.map.entities.splice(i, 1);
+//             }
+//         }
+//     }
+// }
+
+import { Entity } from "./entity.js";
+
+class Boom extends Entity {
     constructor(config) {
+        super(config);
         this.range = config.range || 2; // Needs to config
-        this.x = config.x; // Needs to config
-        this.y = config.y; // Needs to config
-        this.map = config.map; // Needs to config
 
         this.name = "💣";
         this.type = "boom";
@@ -21,19 +172,6 @@ class Boom {
         this.skipUpdate = 0; // 用于跳过更新的tick数
     }
 
-    /**
-     * 初始化的函数
-     */
-    setup() {
-        // 向entities中添加自身
-        this.map.entities.push(this);
-    }
-
-    /**
-     * 负责在新的tick中更新状态的函数
-     * @memberof Boom
-     * @returns {boolean} 是否需要更新地图
-     */
     update() {
         // 跳过更新
         if (this.skipUpdate > 0) {
@@ -64,30 +202,26 @@ class Boom {
      * 炸弹爆炸
      * @memberof Boom
      * @returns {void}
-     * @description 炸弹爆炸后，会在爆炸范围内的所有地块上生成“炸”，然后变成“土”
      */
     boom() {
         this.boomed = true;
         this.boomedInThisTick = true;
         this.name = "";
         this.ticksBrforeBoom = 0;
-        const { blocks, entities } = this.map;
         const { range, x, y } = this;
 
         for (let i = x - range; i <= x + range; i++) {
             for (let j = y - range; j <= y + range; j++) {
-                if (this.map.isInMap(i, j)) {
-                    blocks[i][j].name = "炸";
-                    entities.forEach((entity) => {
-                        // 引爆别的炸弹
-                        if (entity.x === i && entity.y === j) {
-                            if (entity.type === "boom" && !entity.boomed) {
-                                entity.boom();
-                                entity.skipUpdate += 1;
-                            }
-                        }
-                    });
-                }
+                this.game.map.setBlock(i, j, {
+                    name: "炸",
+                });
+                this.game.map.getEntitiesAt(i, j).forEach((entity) => {
+                    // 引爆别的炸弹
+                    if (entity.type === "boom" && !entity.boomed) {
+                        entity.boom();
+                        entity.skipUpdate += 1;
+                    }
+                });
             }
         }
     }
@@ -129,23 +263,22 @@ class Boom {
         for (let x = this.x - this.range; x <= this.x + this.range; x++) {
             for (let y = this.y - this.range; y <= this.y + this.range; y++) {
                 // 如果地块在地图范围内，则在地块上生成“土”
-                if (
-                    this.map.isInMap(x, y) &&
-                    this.map.blocks[x][y].name === "炸"
-                ) {
-                    this.map.blocks[x][y].name = "土";
+                if (this.game.map.getBlock(x, y).name === "炸") {
+                    this.game.map.setBlock(x, y, { name: "土" });
+
+                    // 添加BUD
+                    this.game.bud.queue.push({
+                        x,
+                        y,
+                        delay: 1,
+                        name: "土",
+                    });
                 }
             }
         }
 
         this.finished = true;
-        // 从entities中删除自身
-        for (let i = 0; i < this.map.entities.length; i++) {
-            const entity = this.map.entities[i];
-            if (entity === this) {
-                this.map.entities.splice(i, 1);
-            }
-        }
+        // 从entities中删除自身已经在父类中实现
     }
 }
 
--- js/ecosystem.js
+++ js/ecosystem.js
@@ -1,128 +1,70 @@
 class Eco {
     constructor(config) {
-        this.map = config.map;
-        this.ticker = config.ticker;
+        this.game = config.game;
+        this.bud = config.bud;
 
-        this.taskPriority = 700;
+        // 最大水流距离
+        this.maxWaterFlowDistance = 5;
 
-        this.lastWaterSpreadTick = 0; // 上一次水蔓延距现在的tick数
-        this.waterSpreadInterval = 5; // 水蔓延的间隔
-        this.lastGrassSpreadTick = 0; // 上一次草蔓延距现在的tick数
-        this.grassSpreadInterval = 25; // 草蔓延的间隔
-        this.grassSpreadProbability = 0.1; // 草蔓延的概率
-
-        this.setup = this.setup.bind(this);
-        this.update = this.update.bind(this);
-        this.spreadWater = this.spreadWater.bind(this);
-        this.spreadGrass = this.spreadGrass.bind(this);
+        this.updateWaterFlow = this.updateWaterFlow.bind(this);
     }
 
-    setup() {
-        this.ticker.taskList.push(this);
-    }
+    updateWaterFlow(block) {
+        if (block.name !== "土") {
+            return false;
+        }
+        const neighborBlocks = [-1, 0, 1].flatMap((i) =>
+            [-1, 0, 1]
+                .filter((j) => j !== 0 || i !== 0)
+                .map((j) => {
+                    const newX = block.x + i;
+                    const newY = block.y + j;
+                    return this.game.map.getBlock(newX, newY);
+                })
+        );
 
-    update() {
-        let mapNeedUpdate = false;
-        if (this.lastWaterSpreadTick >= this.waterSpreadInterval) {
-            this.lastWaterSpreadTick = 0;
-            mapNeedUpdate = this.spreadWater();
-        } else {
-            this.lastWaterSpreadTick++;
+        const waterBlocks = neighborBlocks.filter(
+            (block) => block.name === "水"
+        );
+
+        let returnFunc = false;
+        if (waterBlocks.length >= 1) {
+            returnFunc = function (game, block) {
+                game.map.setBlock(block.x, block.y, { name: "水" });
+            };
+        }
+        if (returnFunc) {
+            // 把四方的方块添加到BUD队列
+            [-1, 0, 1].flatMap((i) =>
+                [-1, 0, 1]
+                    .filter((j) => j !== 0 || i !== 0)
+                    .map((j) => {
+                        const newX = block.x + i;
+                        const newY = block.y + j;
+                        const isBlockInQueue = this.bud.queue.some(
+                            (blockInQueue) =>
+                                blockInQueue.x === newX &&
+                                blockInQueue.y === newY
+                        );
+                        if (isBlockInQueue) {
+                            return;
+                        }
+
+                        const blockName = this.game.map.getBlock(
+                            newX,
+                            newY
+                        ).name;
+                        this.bud.queue.push({
+                            x: newX,
+                            y: newY,
+                            delay: 5,
+                            name: blockName,
+                        });
+                    })
+            );
         }
 
-        if (this.lastGrassSpreadTick >= this.grassSpreadInterval) {
-            this.lastGrassSpreadTick = 0;
-            mapNeedUpdate = this.spreadGrass();
-        } else {
-            this.lastGrassSpreadTick++;
-        }
-
-        return mapNeedUpdate;
-    }
-
-    spreadWater() {
-        let mapNeedToBeUpdated = false;
-
-        // 将所有水的new属性设为false
-        this.map.blocks.forEach((row) => {
-            row.forEach((block) => {
-                if (block.name === "水") {
-                    block.new = false;
-                }
-            });
-        });
-
-        // 遍历地图，寻找水
-        this.map.blocks.forEach((row, x) => {
-            row.forEach((block, y) => {
-                if (block.name === "水" && !block.new) {
-                    // 如果该点是水，向四周蔓延
-                    [-1, 0, 1].forEach((i) => {
-                        [-1, 0, 1].forEach((j) => {
-                            if (j !== 0 || i !== 0) {
-                                const newX = x + i;
-                                const newY = y + j;
-                                // 检查该点是否在地图内
-                                if (
-                                    this.map.isInMap(newX, newY) &&
-                                    this.map.blocks[newX][newY].name === "土"
-                                ) {
-                                    this.map.blocks[newX][newY].name = "水";
-                                    this.map.blocks[newX][newY].new = true;
-                                    mapNeedToBeUpdated = true;
-                                }
-                            }
-                        });
-                    });
-                }
-            });
-        });
-
-        return mapNeedToBeUpdated;
-    }
-
-    spreadGrass() {
-        let mapNeedToBeUpdated = false;
-
-        // 将所有草的new属性设为false
-        this.map.blocks.forEach((row) => {
-            row.forEach((block) => {
-                if (block.name === "草") {
-                    block.new = false;
-                }
-            });
-        });
-
-        // 遍历地图，寻找草
-        this.map.blocks.forEach((row, x) => {
-            row.forEach((block, y) => {
-                if (block.name === "草" && !block.new) {
-                    // 如果该点是草，向四周蔓延
-                    [-1, 0, 1].forEach((i) => {
-                        [-1, 0, 1].forEach((j) => {
-                            if (j !== 0 || i !== 0) {
-                                const newX = x + i;
-                                const newY = y + j;
-                                // 检查该点是否在地图内
-                                if (
-                                    this.map.isInMap(newX, newY) &&
-                                    this.map.blocks[newX][newY].name === "土" &&
-                                    Math.random() < this.grassSpreadProbability
-                                ) {
-                                    this.map.blocks[newX][newY].name = "草";
-                                    this.map.blocks[newX][newY].new = true;
-                                    mapNeedToBeUpdated = true;
-                                }
-                            }
-                        });
-                    });
-                } else if (block.new) {
-                    block.new = false;
-                }
-            });
-        });
-
-        return mapNeedToBeUpdated;
+        return returnFunc;
     }
 }
 
